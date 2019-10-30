6e8a037efac2cea80f9126ad373e310623d2c888
outstanding-reference-3248 <omar.benjamin377@gmail.com>
2019-10-30 16:46:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,7 +27,7 @@ def using_concat(array1,array2)
 end
 
 def using_insert(array, item)
-  array.insert(item)
+  array.insert(item, peaches)
 end
 
 def using_uniq(array)
@@ -43,5 +43,5 @@ def using_delete(array, string)
 end
 
 def using_delete_at(array, integer)
-  array.delete(integer)
+  array.delete(integer, 2)
 end
